27e4e43c79d39d0d7f76ec03cbbc6f9e02e03e3f
Pavel Kirienko <pavel.kirienko@gmail.com>
2018-10-22 13:17:04 +0300

    Test coverage analysis

--- test.sh
+++ test.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-all_ok=0
+status=0
 
-mypy --strict --config-file=setup.cfg pydsdl
-all_ok=$(($all_ok + $?))
+if ! mypy --strict --config-file=setup.cfg pydsdl
+then
+    status=1
+fi
 
-pytest --capture=no -vv pydsdl || exit 1
-all_ok=$(($all_ok + $?))
+if coverage run -m pytest --capture=no -vv pydsdl
+then
+    coverage report
+else
+    status=1
+fi
 
-exit $all_ok
+exit $status
