a5d7ac353fbf7dd47a1e23e3e1dfa413eb44a0e2
Pavel Kirienko <pavel.kirienko@gmail.com>
2018-10-31 15:51:14 +0200

    Testing against standard definitions

---
+++ test_namespace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#
+# This is a helper script used for testing the parser against a specified namespace directory.
+# It just directly invokes the corresponding API, prints the output, and exits.
+#
+
+import sys
+import pydsdl
+
+target_directory = sys.argv[1]
+lookup_directories = sys.argv[2:]
+
+output = pydsdl.parse_namespace(target_directory, lookup_directories)
+
+print('\n'.join(map(str, output)))
