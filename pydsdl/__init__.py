f66846b7552cc4db447fb9769249ed81c0e9e030
Pavel Kirienko <pavel.kirienko@gmail.com>
2019-03-22 18:55:07 +0200

    Removed the version module and also not importing the test package because we don't really need it

--- pydsdl/__init__.py
+++ pydsdl/__init__.py
@@ -10,18 +10,20 @@ if sys.version_info[:2] < (3, 5):   # pragma: no cover
     print('A newer version of Python is required', file=sys.stderr)
     sys.exit(1)
 
+__version__ = 0, 4, 0
+__license__ = 'MIT'
+
 # Our unorthodox approach to dependency management requires us to apply certain workarounds.
 # Here, the objective is to allow our library to import stuff from its third-party dependency directory,
 # but at the same time we don't want to interfere with the application that depends on this library.
 # So we modify the import lookup path temporarily while the package initialization is in progress;
-# when done, we restore the path back to its original value. One implication is that it won't be possbile
+# when done, we restore the path back to its original value. One implication is that it won't be possible
 # to import stuff dynamically after the initialization is finished (e.g., function-local imports won't be
 # able to reach the third-party stuff), but we don't care.
 _original_sys_path = sys.path
 sys.path = [os.path.join(os.path.dirname(__file__), 'third_party')] + sys.path
 
 # Never import anything that is not available here - API stability guarantees are only provided for the exposed items.
-# The main entry point of the library.
 from .namespace import read_namespace
 from .namespace import PrintOutputHandler
 
@@ -47,9 +49,5 @@ from .data_type import BitLengthRange, ValueRange, Version
 from .expression import Any
 from .expression import Primitive, Boolean, Rational, String
 from .expression import Container, Set
-
-# Auxiliary items, not related to the business logic and generally not useful.
-from .version import __version__, __license__
-from . import _test as _   # Only for testing, ignore, don't use.
 
 sys.path = _original_sys_path
