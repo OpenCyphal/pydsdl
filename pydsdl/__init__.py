29c5df08bade792e2a8ba8a7bc51bec32aa1e716
Pavel Kirienko <pavel.kirienko@gmail.com>
2019-04-15 13:50:53 +0300

    Version bump

--- pydsdl/__init__.py
+++ pydsdl/__init__.py
@@ -10,7 +10,7 @@ if sys.version_info[:2] < (3, 5):   # pragma: no cover
     print('A newer version of Python is required', file=sys.stderr)
     sys.exit(1)
 
-__version__ = 0, 6, 1
+__version__ = 0, 6, 2
 __license__ = 'MIT'
 
 # Our unorthodox approach to dependency management requires us to apply certain workarounds.
